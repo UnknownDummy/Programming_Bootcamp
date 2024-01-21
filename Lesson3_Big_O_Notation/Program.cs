3aab9557c35940568e1f0f54bac58b0506748b9e
Evgeny <jack9807@yandex.ru>
2024-01-21 18:53:09 +0600

    added lesson3

---
+++ Lesson3_Big_O_Notation/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[, ] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j]);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
+// O(n^2 / 2)
